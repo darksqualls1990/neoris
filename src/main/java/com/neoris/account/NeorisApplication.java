52014dd88a3eaf27348c0d724c159adc342df3a2
darksqualls1990 <infrahector1@gmail.com>
2023-04-18 19:12:54 -0500

    Se suben inicio del proyecto

---
+++ src/main/java/com/neoris/account/NeorisApplication.java
@@ -0,0 +1,20 @@
+package com.neoris.account;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeorisApplication {
+	
+	/**
+	 * Main method to start the spring boot application.
+	 * 
+	 * @param args application  arguments.
+	 */
+	public static void main(String[] args)  
+	{    
+		SpringApplication.run(NeorisApplication.class, args);
+	}   
+
+}
